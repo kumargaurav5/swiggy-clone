921e34622a73fd7b2ff9b54f84d537bf6f328d70
kumargaurav5 <gauravdav2000@gmail.com>
2023-09-22 23:12:38 +0530

    ep3

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-const heading= React.createElement(
-    'h1',{id:"heading1"}, "swiggy clone is "
-    );
 
-console.log(heading)
+//React Element
+//React.createElement =>object =>HTMLElement(render)
+const heading=React.createElement("h1",{id:"heading1"},"swiggy clone")
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+
+//JSX (transpiled before it reaches to js) -Parcel -Babel
+//JSX => React.createElement => ReactElement -JS Object =>HtmlElement(render)
+
+// React Element
+const heading1=
+(
+<h1 id="heading2">
+    Namaste React Using Jsx
+
+</h1>
+);
+
+// React Component => 1. Class component-Old 2. Functional component-New
+//React Functional Component= A js function which returns jsx/react element
+
+const Heading2=()=>{
+    return(
+        <h1 id="heading2">
+            Namaste React Using Jsxgggg
+        </h1>
+    )
+}
+
+//component composition=> nesting of components
+// any js code can be written inside curly braces
+console.log(<Heading2/>)
+root.render(<Heading2/>)
+
